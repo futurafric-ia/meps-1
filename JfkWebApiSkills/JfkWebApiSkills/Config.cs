f187c42c0c8381468142a9b79a8becdf1a36e3f3
QueenieDjamy <64901056+QueenieDjamy@users.noreply.github.com>
2021-02-05 08:30:10 +0000

    Update config

--- JfkWebApiSkills/JfkWebApiSkills/Config.cs
+++ JfkWebApiSkills/JfkWebApiSkills/Config.cs
@@ -4,7 +4,7 @@
     {
         /**************  UPDATE THESE CONSTANTS WITH YOUR SETTINGS  **************/
         public const string AZURE_STORAGE_CONTAINER_NAME = "imagestoreblob";
-        public const string AZURE_SEARCH_INDEX_NAME = "mepsindex";
+        public const string AZURE_SEARCH_INDEX_NAME = "jfkindex";
 
         /*************************************************************************/
 
